a9d7fd7fa54e47e333b056cc92f6ffb2a05fb45b
Kai X <kai01011000i@gmail.com>
2018-05-12 15:12:06 -0400

    Add files via upload

---
+++ DataGenerator.py
@@ -0,0 +1,63 @@
+
+class DataGenerator():
+
+    import keras
+    from keras.preprossessing.image import ImageDataGenerator 
+    import numpy as np
+
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(331,331), n_channels=3, 
+        n_classes=228, shuffle=True):
+        self.dim = dim
+        self.batch_size = batch_size  # Maybe a file that has the appropriate label mapping?
+        self.list_IDs = list_IDs  # The ImageDataGenerator Instance
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+
+    def on_epoch_end(self):
+        #Updates indexes after each epoch
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __getitem(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Geenerate Data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples'
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        #Generate Data
+        for i, ID in enumerate(list_IDs_temp):
+            #store sample
+            X[i,] = np.load('processed_training_data/' + ID + '.npy')
+
+            # Store Class
+            y[i] = self.labels[ID]
+
+
+    def apply_labels(self):
+        # Code to apply labels to each sample based on self.X and self.Y
+
+    def get_next_batch(self):
+        """Get the next training batch"""
+        self.img_data_gen.flow(self.X, self.Y, self.batch_size)
+
+
+img_gen = ImageDataGenerator(...)
+gen = Generator(X, Y, img_gen, 128)
+
+model.fit_generator(gen.get_next_batch(), ...)
