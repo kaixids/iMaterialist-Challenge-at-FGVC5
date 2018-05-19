62df653d24f5366945ec6b74276f166e74eafb6a
Kai X <kai01011000i@gmail.com>
2018-05-19 14:38:58 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -19,31 +19,8 @@ from keras.layers import Activation, Flatten, Dropout, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
 
 from PIL import ImageFile                            
-# Parameters
 
 
-labels = {}
-
-train_ID = []
-for i in tqdm(range(len(df))):
-    #gather list of IDs   
-    img_path = train_path + str(df['imageId'][i]) +'.jpeg'
-    if os.path.isfile(img_path):
-        train_ID.append(str(df['imageId'][i]))
-        labels[str(df['imageId'][i])] = df['labelId'][i]
-        #train_data.append(x)
-
-val_ID = []
-for i in tqdm(range(len(validation))):
-    img_path = validation_path + str(validation['imageId'][i]) +'.jpeg'
-    if os.path.isfile(img_path):
-        val_ID.append('v'+str(validation['imageId']))
-        labels[('v'+str(df['imageId'][i]))] = validation['labelId'][i]
-
-partition['train'] = train_ID
-partition['validation'] = val_ID
-
-# Define a model architecture    
 
 from keras import backend as K
 from sklearn.metrics import hamming_loss
@@ -53,7 +30,8 @@ from sklearn.metrics import hamming_loss
 def hamming_loss(y_true, y_pred):
     return K.mean(y_true * (1 - y_pred) + (1 - y_true) * y_pred)
     
-    
+# Define a model architecture    
+
 base_model = keras.applications.mobilenet.MobileNet(input_shape=(224,224,3), include_top=False, weights='imagenet')
 last = base_model.output
 x = Flatten()(last)
